ddc49848495056809b011d29d7d719b1a40eea83
bkchaluvadi <bcha@atlant3d.com>
2024-02-01 11:59:41 +0100

    feat: grabber video from web cam and send it through the web server

--- SimpleLiveStreamCameraServer/SimpleLiveStreamCameraServer.cpp
+++ SimpleLiveStreamCameraServer/SimpleLiveStreamCameraServer.cpp
@@ -1,20 +1,51 @@
-// SimpleLiveStreamCameraServer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "httplib.h"
+#include <opencv2/opencv.hpp>
+#include <sstream>
 
-#include <iostream>
+int main(void) {
+	httplib::Server svr;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	svr.Get("/video_feed", [&](const httplib::Request&, httplib::Response& res) {
+		res.set_content_provider("multipart/x-mixed-replace; boundary=frame",
+		[&](size_t offset, httplib::DataSink& sink) {
+				cv::VideoCapture camera; // Move the camera object inside the lambda
+
+				// Open the default camera (Webcam) when a client connects
+				if (!camera.open(0)) {
+					std::cerr << "Error opening video stream from webcam" << std::endl;
+					sink.done();
+					return false;
+				}
+
+				while (sink.is_writable()) {
+					cv::Mat frame;
+					camera >> frame; // Capture a frame from the webcam
+
+					if (frame.empty()) {
+						camera.release(); // Release the camera when done
+						sink.done();
+						return false;
+					}
+
+					std::vector<uchar> buffer;
+					cv::imencode(".jpg", frame, buffer);
+					std::stringstream ss;
+					ss << "--frame\r\nContent-Type: image/jpeg\r\n\r\n";
+					ss.write(reinterpret_cast<const char*>(buffer.data()), buffer.size());
+					ss << "\r\n";
+					sink.write(ss.str().c_str(), ss.str().size());
+				}
+
+				camera.release(); // Release the camera when the client disconnects
+				return true;
+			});
+		});
+
+	svr.Get("/", [&](const httplib::Request&, httplib::Response& res) {
+		res.set_content("text/html", "<html><body><img src=\"/video_feed\" /></body></html>");
+		});
+
+	svr.listen("localhost", 8080);
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
